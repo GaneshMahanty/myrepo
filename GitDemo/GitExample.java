445b0fc1b3cf148643f6c7a0079daf8a646c32ea
GaneshMahanty <mohantyganesh1999@gmail.com>
2022-03-08 19:12:58 +0530

    hlw

---
+++ GitDemo/GitExample.java
@@ -0,0 +1,10 @@
+package com.virtusa.git;
+
+
+public class GitExample {
+public static void main(String[] args) {
+	System.out.println("hi team");
+	System.out.println("Hello");
+	System.out.println("hi vijay");
+}
+}
